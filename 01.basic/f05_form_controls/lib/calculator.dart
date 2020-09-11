de795d3f4897b57e9981c6f6f8ea41464963a3be
minlwin <lwin.zawmin@gmail.com>
2020-09-11 12:37:20 +0630

    Update Buttons

--- 01.basic/f05_form_controls/lib/calculator.dart
+++ 01.basic/f05_form_controls/lib/calculator.dart
@@ -20,27 +20,8 @@ class _CalculatorState extends State<Calculator> {
       key: formState,
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Container(
-          child: Padding(
-            padding: EdgeInsets.all(4),
-            child: Column(
-              children: [
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        _formField(digit1, "Digit 1"),
-                        _formField(digit2, "Digit 2"),
-                      ],
-                    ),
-                  ),
-                ),
-                _buttons(),
-                _resultArea()
-              ],
-            ),
-          ),
+        child: Column(
+          children: [_inputs(), Spacer(), _buttons(), Spacer(), _resultArea()],
         ),
       ),
     );
@@ -60,38 +41,52 @@ class _CalculatorState extends State<Calculator> {
 
   Widget _buttons() => Row(
         children: [
-          _calculator("Plus", (a, b) => double.parse(a) + double.parse(b)),
-          _calculator("Minus", (a, b) => double.parse(a) - double.parse(b)),
-          _calculator("Times", (a, b) => double.parse(a) * double.parse(b)),
-          _calculator("Divided", (a, b) => double.parse(a) / double.parse(b)),
+          _button("Plus", (a, b) => double.parse(a) + double.parse(b)),
+          _button("Minus", (a, b) => double.parse(a) - double.parse(b)),
+          _button("Times", (a, b) => double.parse(a) * double.parse(b)),
+          _button("Divided", (a, b) => double.parse(a) / double.parse(b)),
         ],
       );
 
-  Widget _calculator(String sign, Calculate calc) => Container(
-        color: Colors.blue[300],
-        margin: EdgeInsets.all(8),
-        height: 40,
-        child: GestureDetector(
-          onTap: () {
-            if (formState.currentState.validate()) {
-              setState(() {
-                result = calc(digit1.text, digit2.text);
-              });
-            }
-          },
-          child: Center(
-              child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(sign),
-          )),
+  Widget _button(String sign, Calculate calc) => Expanded(
+        child: Container(
+          color: Colors.blue[300],
+          margin: EdgeInsets.all(8),
+          height: 40,
+          child: GestureDetector(
+            onTap: () {
+              if (formState.currentState.validate()) {
+                setState(() {
+                  result = calc(digit1.text, digit2.text);
+                });
+              }
+            },
+            child: Center(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(sign),
+            )),
+          ),
         ),
       );
 
-  Widget _formField(TextEditingController controller, String fieldName) =>
+  Widget _inputs() => Card(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              _input(digit1, "Digit 1"),
+              _input(digit2, "Digit 2"),
+            ],
+          ),
+        ),
+      );
+
+  Widget _input(TextEditingController controller, String fieldName) =>
       TextFormField(
-          controller: controller,
-          decoration: InputDecoration(labelText: "Enter Digit $fieldName"),
-          keyboardType: TextInputType.number,
-          validator: (value) =>
-              value.isEmpty ? "Please enter $fieldName" : null);
+        controller: controller,
+        decoration: InputDecoration(labelText: "Enter Digit $fieldName"),
+        keyboardType: TextInputType.number,
+        validator: (value) => value.isEmpty ? "Please enter $fieldName" : null,
+      );
 }
