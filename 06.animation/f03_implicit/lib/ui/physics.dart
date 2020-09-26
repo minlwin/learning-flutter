d37a0dcc24fa73a1395a977ae5dcc37ee0259a19
minlwin <lwin.zawmin@gmail.com>
2020-09-26 18:31:43 +0630

    Add Animated Size Demo

--- 06.animation/f03_implicit/lib/ui/physics.dart
+++ 06.animation/f03_implicit/lib/ui/physics.dart
@@ -1,14 +1,43 @@
 import 'package:f03_implicit/ui/widgets/template.dart';
 import 'package:flutter/material.dart';
 
-class PhysicsDemo extends StatelessWidget {
+class PhysicsDemo extends StatefulWidget {
   static final String title = "Physics Model";
   static final String route = "/physics";
+
+  @override
+  _PhysicsDemoState createState() => _PhysicsDemoState();
+}
+
+class _PhysicsDemoState extends State<PhysicsDemo> {
+  double _sliderValue = 50;
+
   @override
   Widget build(BuildContext context) {
     return Template(
-      title: title,
-      body: Container(),
+      title: PhysicsDemo.title,
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Center(
+              child: Image.asset("images/jet.png"),
+            ),
+          ),
+          Expanded(
+              child: Column(
+            children: [
+              Text("Slide Here"),
+              Slider(
+                value: _sliderValue,
+                onChanged: (value) {},
+                min: 20,
+                max: 300,
+              )
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
--- 06.animation/f03_implicit/lib/ui/size.dart
+++ 06.animation/f03_implicit/lib/ui/size.dart
@@ -1,14 +1,65 @@
 import 'package:f03_implicit/ui/widgets/template.dart';
 import 'package:flutter/material.dart';
 
-class SizeDemo extends StatelessWidget {
+class SizeDemo extends StatefulWidget {
   static final String title = "Size Demo";
   static final String route = "/size";
+
+  @override
+  _SizeDemoState createState() => _SizeDemoState();
+}
+
+class _SizeDemoState extends State<SizeDemo>
+    with SingleTickerProviderStateMixin {
+  double _sildeValue = 100;
   @override
   Widget build(BuildContext context) {
     return Template(
-      title: title,
-      body: Container(),
+      title: SizeDemo.title,
+      body: Column(
+        children: [
+          Container(
+            child: Expanded(
+              child: Center(
+                child: AnimatedSize(
+                  duration: Duration(milliseconds: 1),
+                  vsync: this,
+                  child: Image.asset(
+                    "images/jet.png",
+                    width: _sildeValue,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 240,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Change Size",
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                Slider(
+                  value: _sildeValue,
+                  onChanged: (value) {
+                    setState(() {
+                      _sildeValue = value;
+                    });
+                  },
+                  min: 30,
+                  max: 300,
+                ),
+                Text(
+                  "Size : ${_sildeValue.toInt()}",
+                  style: Theme.of(context).textTheme.headline5,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
