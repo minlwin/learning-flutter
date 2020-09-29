189267fb00edc5c1f0d1b3c3e6ca1df1d1f67e03
minlwin <lwin.zawmin@gmail.com>
2020-09-29 19:10:37 +0630

    Add Rotation Transition

--- 06.animation/f06_transitions/lib/ui/views/rotation.dart
+++ 06.animation/f06_transitions/lib/ui/views/rotation.dart
@@ -1,14 +1,46 @@
+import 'package:f06_transitions/ui/widgets/control-button.dart';
 import 'package:f06_transitions/ui/widgets/template.dart';
 import 'package:flutter/material.dart';
 
-class RotationDemo extends StatelessWidget {
+class RotationDemo extends StatefulWidget {
   static String route = "/rotation";
   static String title = "Rotation Transition";
+
+  @override
+  _RotationDemoState createState() => _RotationDemoState();
+}
+
+class _RotationDemoState extends State<RotationDemo>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 15))
+          ..repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Template(
-      title: title,
-      body: Container(),
+      title: RotationDemo.title,
+      body: Scaffold(
+        floatingActionButton: ControlButton(controller: _controller),
+        body: Center(
+          child: RotationTransition(
+            turns: _controller.drive(Tween(begin: 1.0, end: 0.0)),
+            child: Container(width: 200, height: 200, color: Colors.blue),
+          ),
+        ),
+      ),
     );
   }
 }
