35d27b7cd91b2067929e4a67861bc9f3032d8e00
minlwin <lwin.zawmin@gmail.com>
2020-09-29 15:16:17 +0630

    Add Size Transition

--- 06.animation/f06_transitions/lib/ui/views/size.dart
+++ 06.animation/f06_transitions/lib/ui/views/size.dart
@@ -1,14 +1,50 @@
+import 'package:f06_transitions/ui/widgets/control-button.dart';
 import 'package:f06_transitions/ui/widgets/template.dart';
 import 'package:flutter/material.dart';
 
-class SizeDemo extends StatelessWidget {
+class SizeDemo extends StatefulWidget {
   static String route = "/size";
   static String title = "Size Transition";
+
+  @override
+  _SizeDemoState createState() => _SizeDemoState();
+}
+
+class _SizeDemoState extends State<SizeDemo>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 3))
+          ..repeat();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Template(
-      title: title,
-      body: Container(),
+      title: SizeDemo.title,
+      body: Scaffold(
+        floatingActionButton: ControlButton(
+          controller: _controller,
+        ),
+        body: Center(
+          child: SizeTransition(
+            sizeFactor: Tween(begin: 1.0, end: 0.0).animate(_controller),
+            child: SizeTransition(
+              sizeFactor: Tween(begin: 0.0, end: 1.0).animate(_controller),
+              axis: Axis.horizontal,
+              child: Container(
+                color: Colors.blue,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
