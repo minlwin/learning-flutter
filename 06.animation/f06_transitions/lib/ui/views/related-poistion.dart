f6d6c5c50dd3a72fad658004e8a38208794d09c9
minlwin <lwin.zawmin@gmail.com>
2020-10-01 23:10:28 +0630

    Add Relative Position Transition

--- 06.animation/f06_transitions/lib/ui/views/related-poistion.dart
+++ 06.animation/f06_transitions/lib/ui/views/related-poistion.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class RelatedPositionDemo extends StatefulWidget {
   static String route = "/related-position";
-  static String title = "Related Positions";
+  static String title = "Relative Positions";
 
   @override
   _RelatedPositionDemoState createState() => _RelatedPositionDemoState();
@@ -18,7 +18,7 @@ class _RelatedPositionDemoState extends State<RelatedPositionDemo>
   void initState() {
     _controller =
         AnimationController(vsync: this, duration: Duration(seconds: 3))
-          ..repeat();
+          ..repeat(reverse: true);
     super.initState();
   }
 
@@ -30,11 +30,34 @@ class _RelatedPositionDemoState extends State<RelatedPositionDemo>
 
   @override
   Widget build(BuildContext context) {
+    final small = 100.0;
+    final large = 200.0;
+
     return Template(
       title: RelatedPositionDemo.title,
       body: Scaffold(
         floatingActionButton: ControlButton(controller: _controller),
-        body: Container(),
+        body: LayoutBuilder(
+          builder: (context, constraints) => Stack(
+            children: [
+              RelativePositionedTransition(
+                rect: RectTween(
+                  begin: Rect.fromLTWH(0, 0, large, large),
+                  end: Rect.fromLTWH(constraints.biggest.width - small,
+                      constraints.biggest.height - small, small, small),
+                ).animate(CurvedAnimation(
+                  parent: _controller,
+                  curve: Curves.elasticInOut,
+                )),
+                size: constraints.biggest,
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: FlutterLogo(),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
